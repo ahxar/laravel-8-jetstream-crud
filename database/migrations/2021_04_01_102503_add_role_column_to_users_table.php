037b9607e5e731520e692232794f0d8bdc1ef0fe
Ahmad Safar <ahmadsafar88@gmail.com>
2021-04-01 18:33:45 +0800

    fix: Add drop role column when revert migration

--- database/migrations/2021_04_01_102503_add_role_column_to_users_table.php
+++ database/migrations/2021_04_01_102503_add_role_column_to_users_table.php
@@ -26,7 +26,7 @@ class AddRoleColumnToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 }
